f95cf2b505a6b360a9db888b8df5b16349239e28
Odirax <dario.lucic@hotmail.de>
2023-05-10 13:03:03 +0200

    Challenge 02

--- challenge_02.js
+++ challenge_02.js
@@ -16,4 +16,14 @@ Alter 13-17 // Cola
 
 /* Getränke-Challenge */
 
+// Deklaration 1
+let name1, alter;
 
+name1 = prompt("Bitte geben sie Ihren Namen ein: ");
+alter = prompt("Bitte geben sie Ihr Alter ein: ");
+
+// Deklaration 2
+let isOlderThan5 = ()
+let isOlderThan6 = ()
+let isOlderThan12 = ()
+let isOlderThan18 = ()
