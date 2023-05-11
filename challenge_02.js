74780bf9a4c05fedd3cb776a15d287fc8c9cd180
Odirax <dario.lucic@hotmail.de>
2023-05-11 08:41:06 +0200

    update

--- challenge_02.js
+++ challenge_02.js
@@ -49,26 +49,26 @@ Alter 13-17 // Cola
 //     console.log(name + " trinkt " + drink + ".");
 //   }
 
- const firstName = prompt("Bitte Namen eingeben:");
-  const age = parseFloat(prompt("Bitte Alter Eingeben:"));
+//  const firstName = prompt("Bitte Namen eingeben:");
+//   const age = parseFloat(prompt("Bitte Alter Eingeben:"));
 
-  // console.log(age);
+//   // console.log(age);
 
-  switch (true) {
-    case (age >= 0) && (age <= 5 ):
-        console.log(firstName + " trinkt Milch.");    
-        break;
-    case  (age >= 6) && (age <= 12 ):
-        console.log(firstName + " trinkt Saft.");    
-        break;
-    case  (age >= 13) && (age <= 17 ):
-        console.log(firstName + " trinkt Cola.");    
-        break;
-    case  (age >= 18) && (age <= 130 ):
-        console.log(firstName + " trinkt Wein.");    
-        break;
-    default:
-        console.log("Bitte Tee trinken.")
-        break;
-  }
+//   switch (true) {
+//     case (age >= 0) && (age <= 5 ):
+//         console.log(firstName + " trinkt Milch.");    
+//         break;
+//     case  (age >= 6) && (age <= 12 ):
+//         console.log(firstName + " trinkt Saft.");    
+//         break;
+//     case  (age >= 13) && (age <= 17 ):
+//         console.log(firstName + " trinkt Cola.");    
+//         break;
+//     case  (age >= 18) && (age <= 130 ):
+//         console.log(firstName + " trinkt Wein.");    
+//         break;
+//     default:
+//         console.log("Bitte Tee trinken.")
+//         break;
+//   }
 
